abfca05f781e6a0e44af0bcf20cd4c74ea8fb693
Manuel Rodriguez <manrodri92@gmail.com>
2019-06-17 01:44:15 +0300

    Added: Basic and advanced types

--- typescript-basics/src/app.ts
+++ typescript-basics/src/app.ts
@@ -60,6 +60,7 @@ const toppings = [
 // }
 // new way
 const order = { pizza, toppings };
+console.log("Object literal example:");
 console.log(order);
 
 // Rest Parameters allows to pass n number of arguments instead (n1,n2,n3,n4,n5,nx) just (...arr)
@@ -73,12 +74,181 @@ function sumAll(message: string, ...arr: Array<number>) {
     console.log(message);
     return arr.reduce((prev, next) => prev + next)
 }
-const sum = sumAll('Rest parameter example!', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+const sum = sumAll('Rest parameter example:', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 console.log(sum);
 
 // Array and object spreads, for inmutable objects will concat two arrays into a single one
-const newToppings = ['Chili', 'Gouda'];
+const newToppings = ['Chili', 'Gouda', 'Corn'];
 const allTopings = [...toppings, ...newToppings];
 console.log('Example of object spread ' + allTopings);
 
+// Destructuring an object
+const justPizza = {
+    name: 'Pepperoni',
+    toppings: ['Pepperoni']
+}
+function orderPizza({ pizzaName, pizzaToppings }: any) {
+    // console.log(name, toppings);
+    return { pizzaName, pizzaToppings }
+}
+const { pizzaName } = orderPizza(justPizza);
 
+// Destructuring an array
+const [first, second, third] = newToppings;
+console.log("Destructuring of an array example " + first, second, third);
+
+// *******************
+// Types in typescript
+
+// Implicit vs explicit type
+let implicitCoupon = 'pizza25';
+let explicitCoupon: string = 'pizza25';
+
+// Number type
+const pizzaCost: number = 10;
+const pizzaToppings: number = 2;
+function calculatePrice(cost: number, toppings: number): number {
+    return (cost + 1.5 * toppings);
+}
+const cost: number = calculatePrice(pizzaCost, pizzaToppings);
+console.log(`Number Type example: \nPizza cost is  ${cost}`);
+
+// String type
+const coupon: string = 'pizza25';
+function normalizeCoupon(code: string): string {
+    return code.toUpperCase();
+}
+const couponMessage: string = `Final coupon is ${normalizeCoupon(coupon)}`;
+console.log("Example of string type: " + couponMessage);
+
+// Boolean type
+const pizzaQuantity: number = 5;
+function offerDiscount(orders: number): boolean {
+    return orders >= 3;
+}
+if (offerDiscount(pizzaQuantity)) {
+    console.log(`Example of boolean type: You're entitled to a discount!`);
+} else {
+    console.log(`Example of boolean type: Order more than 3 pizzas for a discount`);
+}
+
+// Any type we use when we don't know the type o data, let's try to avoid it
+let anyExample: any;
+anyExample = 25;
+anyExample = 'pizza25';
+anyExample = true;
+
+// void type
+let selectedTopping: string = 'Pepperoni';
+function selectTopping(topping: string): void {
+    selectedTopping = topping;
+}
+selectTopping('bacon');
+console.log('Example of void type: ' + selectedTopping);
+
+// Never type is used when the function is never going to return a value
+// function orderError(error: string) {
+//     throw new Error(error);
+//     //Never going to return something
+// }
+// orderError('Never type example: Something went wrong!');
+
+// Null type 
+let nullExample: string | null = 'pizza25';
+function changeNull(): void {
+    nullExample = null;
+}
+console.log('Null type example 1: ' + nullExample);
+changeNull();
+console.log('Null type example 2: ' + nullExample);
+
+// Union types describes a value that can be one of several types
+// Works with boolean string and numbers
+let pizzaSize: string = 'small';
+function selectSize(size: 'small' | 'medium' | 'large'): void {
+    pizzaSize = size;
+}
+selectSize('large');
+console.log(`Example of union type \nPizza size: ${pizzaSize}`);
+
+// Function types
+// name function
+function sumOrder(price: number, quantity: number): number {
+    return price * quantity;
+}
+// arrow function
+let sumOrder2 = (price: number, quantity: number): number => {
+    return price * quantity;
+}
+// Function declared before
+// let sumOrder3:Function;     Not descriptive
+// Better way
+let sumOrder3: (price: number, quantity: number) => number;
+sumOrder3 = (x, y) => x * y;
+// Or   but it's longer and more difficult to read 
+// let sumOrder3: (price: number, quantity: number) => number = (x, y) => x * y;
+const sumName = sumOrder(25, 2);
+const sumArrow = sumOrder2(25, 2);
+const sumShort = sumOrder3(25, 2);
+
+console.log(`Example of a name function \nTotal sum: ${sumName}`);
+console.log(`Example of an arrow function \nTotal sum: ${sumArrow}`);
+console.log(`Example of function previously declared \nTotal sum: ${sumShort}`);
+
+// Optional Parameter
+let sumOrder4: (price: number, quantity?: number) => number;
+sumOrder4 = (x, y) => {
+    if (y) { x * y }
+    return x
+};
+const sumOptional = sumOrder4(25);
+console.log(`Example of function with optional parameter \nTotal sum: ${sumOptional}`);
+
+// Deafult Parameter provides a parameter in the function in case one is not being sent
+let sumOrder5: (price: number, quantity?: number) => number;
+sumOrder5 = (x, y = 1) => x * y
+const sumDefault = sumOrder5(25, 5);
+console.log(`Example of function with default parameter \nTotal sum: ${sumDefault}`);
+
+// Object Type
+let pizzaObject: { name: string; getName(): string } = {
+    name: 'basic pizza',
+    getName() {
+        return pizzaObject.name
+    }
+}
+
+// Array types
+let sizes: string[];
+sizes: ['small', 'medium', 'large'];
+// Generic type
+let toppingsGeneric: Array<string>;
+toppingsGeneric = ['Gouda', 'Chorizo', 'Chicken'];
+// Tupple type tells the order of the types of the parameters entered
+// Useful for a strict data structure
+let pizzaTupple: [string, number, boolean];
+pizzaTupple = ['Hot', 4, true];
+
+// Type Alias
+// We create our own type of virtual type, useful to reuse some duplicated code
+type Size = 'small' | 'medium' | 'large';
+type Callback = (size: Size) => void;
+let pizzaSizeAlias: Size = 'large';
+const selectSizeAlias: Callback = (x) => {
+    pizzaSizeAlias = x;
+};
+selectSizeAlias('medium');
+
+// Type Assertion
+let someValue: any = "this is a string";
+let strLengthOldWay: number = (<string>someValue).length;
+// Or
+let strLengthNewWay: number = (someValue as string).length;
+// Second example with custom type
+type Pizza = { name: string, toppings: number };
+const PizzaAssertion: Pizza = { name: 'Spicy', toppings: 2 };
+const serialized = JSON.stringify(PizzaAssertion);
+function getNameFromJSON(obj: string) {
+    // Using assertion we access the properties in a JSOn for a pizza type
+    return (JSON.parse(obj) as Pizza).name;
+}
