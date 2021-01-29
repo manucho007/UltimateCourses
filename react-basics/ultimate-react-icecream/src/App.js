0a5cd67dcfde151393c5c5e63464e2453b0bceed
Manuel Rodriguez <manrodri92@gmail.com>
2021-01-29 13:23:26 +0300

    Ultimate Icecream project initial commit

---
+++ react-basics/ultimate-react-icecream/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/ice-cream.scss';
+
+const App = () => {
+  return (
+    <header>
+      <h1>Your app goes here</h1>
+    </header>
+  );
+};
+
+export default App;
