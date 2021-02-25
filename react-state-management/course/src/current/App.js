cdb3933636cd9aba386f80640ec0f0e0ae336d8b
Manuel Rodriguez <manrodri92@gmail.com>
2021-02-25 12:03:39 +0300

    Updated apollo/client

--- react-state-management/course/src/current/App.js
+++ react-state-management/course/src/current/App.js
@@ -1,5 +1,23 @@
-import React, { useCallback, useState } from 'react';
+import React from 'react';
+import { ApolloProvider } from '@apollo/client';
+import { client } from './client';
+import { useExchangeRates } from './useExchangeRates';
 
-export const App = () => {
-  return <div />;
+const App = () => {
+  let { data, loading } = useExchangeRates();
+
+  if (loading) return 'loading...';
+  if (!data) return 'no data... :( ';
+
+  return data.rates.map((rate) => (
+    <div key={rate.currency}>
+      {rate.currency}: {rate.rate}
+    </div>
+  ));
 };
+
+export const AppContainer = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
